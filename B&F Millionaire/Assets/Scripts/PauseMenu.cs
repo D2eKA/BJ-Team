f7a441c0c45cd218c5b6845f08fb85e5f45dd63a
Денис Куковский <kukovskii@sfedu.ru>
2024-12-01 18:10:21 +0300

    add pause menu

---
+++ B&F Millionaire/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pause_menu;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause_menu.activeSelf)
+                Continue();
+            else
+                Pause();
+        }
+    }
+    public void Continue()
+    {
+        pause_menu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    public void Pause()
+    {
+        pause_menu.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+    public void Exit()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
