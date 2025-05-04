051dd1a9edc400b862a69fb39852dd2d4f01e2d6
Денис Куковский <kukovskii@sfedu.ru>
2025-05-04 21:05:19 +0300

    fix

--- B&F Millionaire/Assets/Scripts/PauseMenu.cs
+++ B&F Millionaire/Assets/Scripts/PauseMenu.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class PauseMenu : MonoBehaviour
 {
